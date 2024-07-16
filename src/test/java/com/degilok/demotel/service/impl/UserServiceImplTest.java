e6c59ea3f35fabedd4106003ebd6514a35b0e8d4
zensld11 <baktvbekovaadelina@gmail.com>
2024-07-16 09:19:22 +0600

    messageController one method worked

---
+++ src/test/java/com/degilok/demotel/service/impl/UserServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.degilok.demotel.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    @Autowired
+    private UserServiceImpl sut;
+
+    @Test
+    void getUserByLogin() {
+        sut.getUserByLogin("Adelina");
+    }
+
+}
